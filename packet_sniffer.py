02ac97a1a2c31aa85166eb663e6db56d0bc6a357
gmful <gmfullenkamp@gmail.com>
2023-01-18 20:39:18 -0500

    Added a linux script for creating wifi beacons and updated the packet sniffer to get queries from packets sent from your machine.

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,3 +1,23 @@
-from scapy.all import *
+"""
+Must install wireshark in order for winpcap to be installed correctly.
+"""
+from scapy.all import sniff
 
-capture = sniff(prn=lambda x: x.summary(), filter="tcp")
+COUNT = 999999
+
+queries = []
+
+
+# Gets query specific packets
+def get_queries(p: str):
+    try:
+        qry = p.payload.payload.payload.raw_packet_cache_fields["qd"].fields["qname"]
+        if qry not in queries:
+            queries.append(qry)
+            print("\t", qry)
+    except:
+        pass
+
+
+print("Sniffed queries:")
+capture = sniff(prn=lambda x: get_queries(x), count=COUNT)

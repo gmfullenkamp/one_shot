90be052dc4cfe5f6e78824a14e2c1fc269a8e589
Fullenkamp <gmfullenkamp@gmail.com>
2023-01-24 10:29:58 -0500

    Added a state printer, the initial goal, and start state.

--- mystic_puzzle_search_tree.py
+++ mystic_puzzle_search_tree.py
@@ -1,3 +1,36 @@
+"""
+3x3 mystic puzzle game.
+Goal state: [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+1 2 3
+4 5 6
+7 8 0
+"""
+import numpy as np
+import random
+
+ALL_TILES = [1, 2, 3, 4, 5, 6, 7, 8, 0]
+BOARD_SHAPE = [3, 3]
+
+goal_state = np.reshape(ALL_TILES, BOARD_SHAPE).tolist()
+random.shuffle(ALL_TILES)
+start_state = np.reshape(ALL_TILES, BOARD_SHAPE).tolist()
+
+
+def state_print(state: list) -> None:
+    """Prints the state list."""
+    for row in state:
+        p = ""
+        for tile in row:
+            p += str(tile) + " "
+        print(p)
+
+
+print("Goal state:")
+state_print(goal_state)
+print("Start state:")
+state_print(start_state)
+
+
 class NonBinTree:
 
     def __init__(self, val):
@@ -18,3 +51,5 @@ a.add_node(4)
 a.nodes[2].add_node(2)
 
 print(a)
+# TODO: Limited breadth first search for the goal
+# TODO: Custom heuristic addition (loss?!?!?!)

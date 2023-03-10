443b03d53f660536690c79d4163d487dbebbaf76
Fullenkamp <gmfullenkamp@gmail.com>
2023-03-10 09:42:29 -0500

    Deleted old json and updated macro to save screen ratios instead of specific pixel values for better cross platform use of the macro.json files.

--- auto_make_macro.py
+++ auto_make_macro.py
@@ -1,23 +1,26 @@
 import json
+import pyautogui as pag
 from pynput import mouse
 import time
 
-output_filename = "auto_btd5.json"
+output_filename = "auto_btd6.json"
 
 start_time = time.time()
-actions = {"widths": [], "heights": [], "times": []}
+actions = {"width_ratios": [], "height_ratios": [], "times": []}
+screen_width, screen_height = pag.size()
 
 
-# TODO: Later have it save screen ratios instead of specific pixel values
 def on_click(x, y, button, pressed):
     if button == mouse.Button.left and pressed is True:  # When left mouse click (track)
         s = time.time() - start_time
-        print(f"left mouse pressed at ({x}, {y}, {s})")
-        actions["widths"].append(x)
-        actions["heights"].append(y)
+        print(f"left mouse pressed at ({x / screen_width}, {y / screen_height}, {s})")
+        actions["width_ratios"].append(x / screen_width)
+        actions["height_ratios"].append(y / screen_height)
         actions["times"].append(s)
+    # TODO: Add another dict field for button pressed (left or right) and change this to be a specific hotkey or smthn
     if button == mouse.Button.right and pressed is True:  # When right mouse click (save data)
         json_object = json.dumps(actions)
+        print("Saving locations so far...")
         with open(output_filename, 'w') as f:
             f.write(json_object)
 
--- auto_play_macro.py
+++ auto_play_macro.py
@@ -2,19 +2,21 @@ import json
 import pyautogui as pag
 from tqdm import tqdm
 
-input_filename = "auto_btd5.json"
+input_filename = "auto_btd6.json"
+
+screen_width, screen_height = pag.size()
 
 f = open(input_filename)
 actions = json.load(f)
 f.close()
 
-widths, heights, times = actions["widths"], actions["heights"], actions["times"]
+width_ratios, height_ratios, times = actions["width_ratios"], actions["height_ratios"], actions["times"]
 count = 1
 while True:
     print(f"Starting loop {count}...")
     prev_s = 0
-    for x, y, s in tqdm(zip(widths, heights, times)):
-        pag.moveTo(x, y, duration=s - prev_s)
+    for x_ratio, y_ratio, s in tqdm(zip(width_ratios, height_ratios, times)):
+        pag.moveTo(screen_width * x_ratio, screen_height * y_ratio, duration=s - prev_s)
         pag.click()
         prev_s = s
     count += 1
